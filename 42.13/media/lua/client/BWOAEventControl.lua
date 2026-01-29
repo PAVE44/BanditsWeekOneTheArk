db693652f51b7b26d31f40f11be3176ece5d6814
ppawlowski <ppawlowski@qualtrics.com>
2026-01-29 12:41:07 +0100

    sandbox option for POI

--- 42.13/media/lua/client/BWOAEventControl.lua
+++ 42.13/media/lua/client/BWOAEventControl.lua
@@ -77,12 +77,14 @@ local function everyOneMinute()
             if BanditUtils.DistTo(px, py, event.x, event.y) < 50 then
                 BWOAPlaceEvents.Render(event)
 
-                BWOASound.PlayPlayer({sound="AngelProximity"})
+                if SandboxVars.BWOA.AngelProximity then
+                    BWOASound.PlayPlayer({sound="AngelProximity"})
 
-                local icon = "media/ui/defend.png"
-                local color = {r=0.5, g=0.5, b=1}
-                local desc = "Point of Interest to Discover"
-                BanditEventMarkerHandler.set(getRandomUUID(), icon, 7200, event.x, event.y, color, desc)
+                    local icon = "media/ui/defend.png"
+                    local color = {r=0.5, g=0.5, b=1}
+                    local desc = "Point of Interest to Discover"
+                    BanditEventMarkerHandler.set(getRandomUUID(), icon, 7200, event.x, event.y, color, desc)
+                end
             end
         end
     end
